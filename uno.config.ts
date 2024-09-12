ef33e173ba3060e9016694b43ec4840a753b4617
asjones-code <64913351+asjones-code@users.noreply.github.com>
2024-09-12 14:18:15 -0400

    Pushed from air
    sections

--- uno.config.ts
+++ uno.config.ts
@@ -2,31 +2,33 @@
 import presetWind from '@unocss/preset-wind'
 
 import {
-    defineConfig,
-    presetTypography,
-    presetWebFonts,
-  } from 'unocss'
+  defineConfig,
+  presetTypography,
+  presetWebFonts,
+} from 'unocss'
 
-  
-  export default defineConfig({
-    shortcuts: [
+
+export default defineConfig({
+
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
       // ...
-    ],
-    theme: {
-      colors: {
-        // ...
-      }
-    },
-    presets: [
-      presetTypography(),
-      presetWind(),
-      presetWebFonts({
-        fonts: {
-          // ...
-        },
-      }),
-    ],
-    transformers: [
+    }
+  },
+  presets: [
+    presetTypography(),
+    presetWind(),
+    presetWebFonts({
+      fonts: {
+        geo: 'Geologica',
+        mono: ['Fira Code', 'Fira Mono:400,700'],
+      },
+    }),
+  ],
+  transformers: [
 
-    ],
-  })
+  ],
+})
