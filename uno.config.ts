6942e7f50ae815d1848a66139bdfbfcc9d9a174c
asjones-code <64913351+asjones-code@users.noreply.github.com>
2024-09-19 08:45:36 -0400

    styling

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,6 @@
 // uno.config.ts
 import presetWind from '@unocss/preset-wind'
+import { presetFluid } from 'unocss-preset-fluid'
 
 import {
   defineConfig,
@@ -19,14 +20,45 @@ export default defineConfig({
     }
   },
   presets: [
+    presetFluid({
+      maxWidth: 1440,
+      minWidth: 375,
+      extendMaxWidth: null,
+      extendMinWidth: null,
+      remBase: 16,
+      useRemByDefault: false,
+      ranges: null,
+      commentHelpers: false,
+    }),
     presetTypography(),
     presetWind(),
     presetWebFonts({
+      provider: 'fontshare',
       fonts: {
-        geo: 'Geologica',
+        geo:  [
+          {
+            name: 'Geologica',
+            weights: ['400', '700'],
+            italic: true,
+            provider: 'google',
+          },
+          
+        ],
+        tabular:  [
+          {
+            name: 'Tabular',
+            weights: ['100', '400', '700'],
+            italic: false,
+            provider: 'fontshare',
+          },
+          
+        ],
+        excon: ['Excon', 'Excon:100,300,400,700,900'],
         mono: ['Fira Code', 'Fira Mono:400,700'],
       },
-    }),
+    },
+    
+  ),
   ],
   transformers: [
 
