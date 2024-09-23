1332588e6be578ae04bd4bbf10e8037959f8d5a4
asjones-code <64913351+asjones-code@users.noreply.github.com>
2024-09-23 16:07:00 -0500

    horizontal scroll v2

--- src/js/hoscroll.js
+++ src/js/hoscroll.js
@@ -4,6 +4,10 @@ import LocomotiveScroll from 'locomotive-scroll';
 import Lenis from "lenis";
 
 gsap.registerPlugin(ScrollTrigger);
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
 
 const produtoSection = document.querySelector(".produto_section");
 let painel = document.querySelector(".painel");
@@ -34,3 +38,38 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
+
+function smoothMoves(event) {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  gsap.to(circle, { duration: 1.5, attr: { cx: mouseX, cy: mouseY } });
+}
+
+function expandHero() {
+  tl.to(circle, {
+    duration: 1,
+    attr: {
+      r: "1000"
+    },
+    ease: "power2.inOut"
+  })
+    .to(
+      hero,
+      {
+        duration: 1,
+        scale: 1,
+        borderRadius: 0,
+        ease: "power2.inOut"
+      },
+      "-=0.5"
+    )
+    .to(body, {
+      overflow: "auto"
+    });
+}
+
+document.addEventListener("mousemove", smoothMoves);
+hero.addEventListener("click", expandHero);
