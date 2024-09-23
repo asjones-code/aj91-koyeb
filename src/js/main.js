172ec91988bf67644b9c9cc357e7eaa770e117db
asjones-code <64913351+asjones-code@users.noreply.github.com>
2024-09-22 17:29:50 -0700

    ai boilerplate

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import * as THREE from "three";
+
+window.addEventListener("load", function () {
+  start();
+});
+
+async function start() {
+  document.write("Hello world!");
+  console.log(THREE);
+}
