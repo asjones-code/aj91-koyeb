830c164584d456aa0deda8c1abf336bf93cb247b
asjones-code <64913351+asjones-code@users.noreply.github.com>
2024-09-26 14:49:31 -0400

    try

--- dist/index.6ddd2e47.js
+++ dist/index.6ddd2e47.js
@@ -586,12 +586,8 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"68Yh6":[function(require,module,exports) {
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let interval = null;
-// Store the original value (text) inside the h4 tag
-const originalText = document.querySelector("h4").innerText;
 document.querySelector("h4").onmouseover = (event)=>{
     let iteration = 0;
-    // Change data-value to the new text when hovering
-    event.target.setAttribute("data-value", "MOUSEOVER"); // New hover text
     clearInterval(interval);
     interval = setInterval(()=>{
         event.target.innerText = event.target.innerText.split("").map((letter, index)=>{
@@ -599,18 +595,6 @@ document.querySelector("h4").onmouseover = (event)=>{
             return letters[Math.floor(Math.random() * 26)];
         }).join("");
         if (iteration >= event.target.dataset.value.length) clearInterval(interval);
-        iteration += 1 / 3;
-    }, 30);
-};
-document.querySelector("h4").onmouseleave = (event)=>{
-    let iteration = 0;
-    clearInterval(interval);
-    interval = setInterval(()=>{
-        event.target.innerText = event.target.innerText.split("").map((letter, index)=>{
-            if (index < iteration) return originalText[index]; // Return to original text on mouse leave
-            return letters[Math.floor(Math.random() * 26)];
-        }).join("");
-        if (iteration >= originalText.length) clearInterval(interval);
         iteration += 1 / 3;
     }, 30);
 };
--- src/js/letters.js
+++ src/js/letters.js
@@ -1,55 +1,28 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let interval = null;
 
-// Store the original value (text) inside the h4 tag
-const originalText = document.querySelector("h4").innerText;
+let interval = null;
 
 document.querySelector("h4").onmouseover = event => {  
   let iteration = 0;
   
-  // Change data-value to the new text when hovering
-  event.target.setAttribute("data-value", "MOUSEOVER"); // New hover text
-
   clearInterval(interval);
-
+  
   interval = setInterval(() => {
     event.target.innerText = event.target.innerText
       .split("")
       .map((letter, index) => {
-        if (index < iteration) {
+        if(index < iteration) {
           return event.target.dataset.value[index];
         }
-        return letters[Math.floor(Math.random() * 26)];
+      
+        return letters[Math.floor(Math.random() * 26)]
       })
       .join("");
     
-    if (iteration >= event.target.dataset.value.length) { 
+    if(iteration >= event.target.dataset.value.length){ 
       clearInterval(interval);
     }
     
     iteration += 1 / 3;
   }, 30);
-};
-
-document.querySelector("h4").onmouseleave = event => {
-  let iteration = 0;
-  clearInterval(interval);
-
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if (index < iteration) {
-          return originalText[index];  // Return to original text on mouse leave
-        }
-        return letters[Math.floor(Math.random() * 26)];
-      })
-      .join("");
-
-    if (iteration >= originalText.length) {
-      clearInterval(interval);
-    }
-
-    iteration += 1 / 3;
-  }, 30);
-};
+}
